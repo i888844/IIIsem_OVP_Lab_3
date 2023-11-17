62f1695b2d8bcf22fd638814b2f03ece0b8b6024
i888844 <i888844@yandex.ru>
2023-11-17 09:14:02 +0300

    2nd commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,12 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -64,11 +70,71 @@
             this.dataGridView1.Size = new System.Drawing.Size(2677, 1289);
             this.dataGridView1.TabIndex = 2;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(418, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ввод элемента матрицы:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(627, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(710, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 26);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(912, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 40);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Принять";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(2397, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(194, 40);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Сбросить все данные";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 1367);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(2704, 1573);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button1);
@@ -77,6 +143,7 @@
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -85,6 +152,12 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -13,13 +13,17 @@ namespace Lab._3
 {
     public partial class Form1 : Form
     {
+        private int i = 0;
+        private int j = 0;
         private int columnsFromForm2;
         private int stringsFromForm2;
         private int[,] matrix;
+
         public Form1()
         {
             InitializeComponent();
         }
+
         private void button1_Click(object sender, EventArgs e)
         {
             var result = MessageBox.Show(
@@ -31,6 +35,7 @@ namespace Lab._3
                 Close();
             }
         }
+
         private void button2_Click(object sender, EventArgs e)
         {
             using (Form2 inputDialog = new Form2())
@@ -39,29 +44,126 @@ namespace Lab._3
                 inputDialog.ShowDialog();
             }
         }
+
         private void Form2_ButtonClicked(object sender, EventArgs e)
         {
             if (sender is Form2 form2)
             {
+                i = 0;
+                j = 0;
                 columnsFromForm2 = form2.Columns;
                 stringsFromForm2 = form2.Strings;
                 matrix = new int[columnsFromForm2, stringsFromForm2];
                 dataGridView1.Rows.Clear();
                 dataGridView1.Columns.Clear();
-                for (int i = 0; i < columnsFromForm2; i++)
+                for (int column = 0; column < columnsFromForm2; column++)
                 {
-                    dataGridView1.Columns.Add($"Column{i + 1}", $"Column{i + 1}");
+                    dataGridView1.Columns.Add($"Column{column + 1}", $"Column{column + 1}");
                 }
-                for (int j = 0; j < stringsFromForm2; j++)
+                for (int row = 0; row < stringsFromForm2; row++)
                 {
-                    DataGridViewRow row = new DataGridViewRow();
-                    for (int i = 0; i < columnsFromForm2; i++)
+                    DataGridViewRow dataGridViewRow = new DataGridViewRow();
+                    dataGridViewRow.CreateCells(dataGridView1);
+                    for (int column = 0; column < columnsFromForm2; column++)
                     {
-                        row.Cells.Add(new DataGridViewTextBoxCell { Value = matrix[i, j] });
+                        dataGridViewRow.Cells[column].Value = matrix[column, row];
                     }
-                    dataGridView1.Rows.Add(row);
+                    dataGridView1.Rows.Add(dataGridViewRow);
+                }
+                i = 0;
+                j = 0;
+                label2.Text = $"[{i}][{j}]";
+            }
+        }
+
+        private void ProcessMatrix()
+        {
+            int sum = 0;
+            int count = 0;
+            int max = int.MinValue;
+            StringBuilder matrixStringBuilder = new StringBuilder();
+            for (int row = 0; row < stringsFromForm2; row++)
+            {
+                for (int column = 0; column < columnsFromForm2; column++)
+                {
+                    matrixStringBuilder.Append(matrix[column, row]);
+                    if (column < columnsFromForm2 - 1)
+                    {
+                        matrixStringBuilder.Append("   ");
+                    }
+                }
+                matrixStringBuilder.AppendLine();
+            }
+            label3.Text = matrixStringBuilder.ToString();
+            for (int row = 0; row < stringsFromForm2; row++)
+            {
+                for (int column = 0; column < columnsFromForm2; column++)
+                {
+                    sum += matrix[column, row];
+                    count++;
+                    if (matrix[column, row] > max)
+                    {
+                        max = matrix[column, row];
+                    }
                 }
             }
+            double average = (double)sum / count;
+            int sumLessThanAverage = 0;
+            for (int row = 0; row < stringsFromForm2; row++)
+            {
+                for (int column = 0; column < columnsFromForm2; column++)
+                {
+                    if (matrix[column, row] < average)
+                    {
+                        sumLessThanAverage += matrix[column, row];
+                    }
+                }
+            }
+            if (sumLessThanAverage > 0)
+            {
+                for (int row = 0; row < stringsFromForm2; row++)
+                {
+                    for (int column = 0; column < columnsFromForm2; column++)
+                    {
+                        matrix[column, row] /= max;
+                        dataGridView1[column, row].Value = matrix[column, row].ToString();
+                    }
+                }
+            }
+            matrixStringBuilder.AppendLine($"Среднее арифметическое: {average}");
+            matrixStringBuilder.AppendLine($"Максимальное значение: {max}");
+            matrixStringBuilder.AppendLine($"Сумма элементов меньше среднего арифметического: {sumLessThanAverage}");
+            label3.Text = matrixStringBuilder.ToString();
+        }
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int cellValue))
+            {
+                matrix[i, j] = cellValue;
+                dataGridView1[i, j].Value = cellValue.ToString();
+            }
+            textBox1.Clear();
+            if (++i >= columnsFromForm2)
+            {
+                i = 0;
+                if (++j >= stringsFromForm2)
+                {
+                    ProcessMatrix();
+                }
+            }
+            label2.Text = $"[{i}][{j}]";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            i = 0;
+            j = 0;
+            columnsFromForm2 = 0;
+            stringsFromForm2 = 0;
+            matrix = null;
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            label2.Text = $"[{i}][{j}]";
         }
     }
 }
